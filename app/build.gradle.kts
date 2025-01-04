555558cf1fe5572f9d797179b7ea3e2493dec3f5
Sargun Vohra <sargun.vohra@gmail.com>
2025-01-04 15:17:12 -0800

    no lwjgl plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,27 +1,42 @@
-import org.lwjgl.Lwjgl
-import org.lwjgl.Release
-import org.lwjgl.lwjgl
-import org.lwjgl.sonatype
-
 plugins {
     kotlin("jvm") version "2.0.21"
     application
-    id("org.lwjgl.plugin") version "0.0.35"
 }
 
 repositories {
     mavenCentral()
-    sonatype()
 }
 
 dependencies {
-    lwjgl {
-        version = Release.latest
-        implementation(Lwjgl.Preset.minimalOpenGL + Lwjgl.Module.jawt)
-    }
+    implementation(platform("org.lwjgl:lwjgl-bom:3.3.5"))
+    implementation("org.lwjgl", "lwjgl")
+    implementation("org.lwjgl", "lwjgl-jawt")
+    implementation("org.lwjgl", "lwjgl-opengl")
+    implementation("org.lwjgl", "lwjgl-vulkan")
+
     implementation("org.lwjglx:lwjgl3-awt:0.2.3") {
         exclude(group = "org.lwjgl")
     }
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-linux")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-linux")
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-linux-arm64")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-linux-arm64")
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-macos")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-macos")
+    runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = "natives-macos")
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-macos-arm64")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-macos-arm64")
+    runtimeOnly("org.lwjgl", "lwjgl-vulkan", classifier = "natives-macos-arm64")
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-windows")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-windows")
+
+    runtimeOnly("org.lwjgl", "lwjgl", classifier = "natives-windows-arm64")
+    runtimeOnly("org.lwjgl", "lwjgl-opengl", classifier = "natives-windows-arm64")
 }
 
 java {
